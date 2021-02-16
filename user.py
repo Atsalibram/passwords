1c041f2bdd97586a80ff71cbac5e7b62ee56ec23
atsalibram <atsalibaram@gmail.com>
2021-02-16 15:44:15 +0300

    added def function

--- user.py
+++ user.py
@@ -2,4 +2,10 @@ class user:
     '''
     class that generates new instance of user
     '''
-    
+
+    def __init__(self, first_name, last_name, email, user_password):
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.user_password = user_password
